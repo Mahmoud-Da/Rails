d284d74e730dfc1bc277097fc9c6c75445e5c759
Mahmoud-Da <mdabbagh6@gmail.com>
2024-12-17 22:43:07 +0900

    lesson3_2

--- cham/lesson3/rails-app/db/migrate/20241214083212_add_publisher_to_articles.rb
+++ cham/lesson3/rails-app/db/migrate/20241214083212_add_publisher_to_articles.rb
@@ -1,4 +1,4 @@
-class AddPublisherToArticle < ActiveRecord::Migration[8.0]
+class AddPublisherToArticles < ActiveRecord::Migration[8.0]
   def change
     add_column :articles, :publisher, :string
   end
