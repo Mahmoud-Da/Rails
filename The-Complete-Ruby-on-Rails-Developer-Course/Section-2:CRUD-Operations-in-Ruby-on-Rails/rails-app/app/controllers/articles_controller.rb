283e666674dfe49aa131e89f73d6bec988f84303
Mahmoud-Da <mdabbagh6@gmail.com>
2024-12-29 15:34:24 +0900

    11-Edit and update: update existing articles

--- The-Complete-Ruby-on-Rails-Developer-Course/Section-2:CRUD-Operations-in-Ruby-on-Rails/rails-app/app/controllers/articles_controller.rb
+++ The-Complete-Ruby-on-Rails-Developer-Course/Section-2:CRUD-Operations-in-Ruby-on-Rails/rails-app/app/controllers/articles_controller.rb
@@ -11,17 +11,30 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  
+  def edit
+     @article = Article.find(params[:id])
+  end
 
-  def create
-    
+  def create 
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
       flash[:notice] = "Article was created successfully"
       redirect_to @article
     else 
       flash[:alert] = "Article created failed"
-      puts @article.errors.full_messages
       render :new
+    end
+  end
+
+  def update 
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else 
+      flash[:alert] = "Article updated failed"
+      render :edit
     end
   end
 end
